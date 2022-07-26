84098cf64d98eae077d6d3ca8a452904cf363898
Fayesg <mattvinicius.contato@gmail.com>
2022-07-25 21:17:12 -0300

    RobotRafactor/ add show phrases method and show a phrase

--- Robot.py
+++ Robot.py
@@ -38,25 +38,36 @@ class Robot(Appliance):
         if self.power == "On":
             self.__phrases.append(phrase for phrase in args)
 
-    def charge(self):
-        if self.battery < 100:
-            self.__battery = 100
-            return f"Gimme a minute to charge...\n DONE"
+    def say_robot_name(self):
+        if self.power == "On":
+            self.__battery -= 1
+            return f"Hi, Nice to meet you\nMy name is {self.name}"
         else:
-            return f"I'm fully charged"
+            return "..."
+
+    def charge(self):
+        if self.power == "On":
+            if self.battery < 101:
+                self.__battery = 101
+                return f"Gimme a minute to charge...\n DONE"
+            else:
+                return f"I'm fully charged"
+        else:
+            return '...'
 
     def battery_status(self):
-        self.__battery -= 1
-        if self.battery <= 25 and self.power == "On":
-            return f"Battery: {self.battery}%\n" \
-                   f"I think i might need a little charge"
+        if self.power == "On":
+            self.__battery -= 1
+            if self.battery <= 25:
+                return f"Battery: {self.battery}%\n" \
+                       f"I think i might need a little charge"
 
-        elif self.battery < 100 and self.power == "On":
-            return f"Battery: {self.battery}%\n"
+            elif self.battery < 100:
+                return f"Battery: {self.battery}%\n"
 
-        elif self.battery >= 100 and self.power == "On":
-            return f"Battery: {self.battery}%\n" \
-                   f"I'm full!"
+            elif self.battery >= 100:
+                return f"Battery: {self.battery}%\n" \
+                       f"I'm full!"
         else:
             return f"..."
 
@@ -75,9 +86,24 @@ class Robot(Appliance):
             else:
                 return f'...'
 
-    def say_robot_name(self):
+    def show_phrases(self):
         if self.power == "On":
-            self.__battery -= 1
-            return f"Hi, Nice to meet you\nMy name is {self.name}"
+            self.__battery -= 3
+            print('I know these phrases:\n')
+            for index in range(len(self.phrases)):
+                print(f'|{index + 1}|{self.phrases[index]}|')
+            return f'Done'
         else:
-            return "..."
+            return '...'
+
+    def show_a_phrase(self, phrase_number):
+        if self.power == 'On':
+            self.__battery -= 3
+            for index in range(len(self.phrases)):
+                if index == phrase_number - 1:
+                    return f'{self.phrases[index]}'
+                else:
+                    return f'Phrase not found!\nbe sure you put the right phrase number...'
+        else:
+            return '...'
+
--- Robot_test.py
+++ Robot_test.py
@@ -4,121 +4,245 @@ from Robot import Robot
 
 
 class RobotTest(unittest.TestCase):
-    # Testing Say robot name - Power On
+
+    def setUp(self):
+        self.robot_power_on_battery_full = Robot("Jeff", True, 101, phrases=["PhraseOne"])
+        self.robot_power_on_battery_average = Robot("Jeff", True, 50, phrases=["PhraseOne"])
+        self.robot_power_on_battery_lower = Robot("Jeff", True, 25, phrases=["PhraseOne"])
+        self.robot_memory_full_power_on = Robot("Jeff", True, 101, phrases=[
+            "PhraseOne",
+            "PhraseTwo",
+            "PhraseThree",
+            "PhraseFour",
+            "PhraseFive",
+            "PhraseSix"])
+
+        self.robot_power_off_battery_full = Robot("Jeff", False, 101, phrases=["PhraseOne"])
+        self.robot_power_off_battery_average = Robot("Jeff", False, 50, phrases=["PhraseOne"])
+        self.robot_power_off_battery_lower = Robot("Jeff", False, 25, phrases=["PhraseOne"])
+        self.robot_memory_full_power_off = Robot("Jeff", False, 101, phrases=[
+            "PhraseOne",
+            "PhraseTwo",
+            "PhraseThree",
+            "PhraseFour",
+            "PhraseFive",
+            "PhraseSix"])
+
+    # Testing Say robot name - Power On -------------------------------------------------------
     def test_say_robot_name_on(self):
         self.assertEqual(
-            Robot("Jeff", True, 101).say_robot_name(),
+            self.robot_power_on_battery_full.say_robot_name(),
             "Hi, Nice to meet you\nMy name is Jeff"
         )
-
-    # Testing Say Robot name - Power Off
-    def test_say_robot_name_off(self):
         self.assertEqual(
-            Robot("Jeff", False, 101).say_robot_name(),
-            "..."
+            self.robot_power_on_battery_full.battery, 100
         )
 
-    # Testing Charge Method
+    # Testing Say Robot name - Power Off --------------------------------------------------------
+    def test_say_robot_name_off(self):
+        self.assertEqual(
+            self.robot_power_off_battery_full.say_robot_name(),
+            "..."
+        )
+        self.assertEqual(
+            self.robot_power_off_battery_full.battery, 101
+        )
+
+    # Testing Charge Method - Power ON ---------------------------------------------------------
     def test_charge_when_battery_full(self):
         self.assertEqual(
-            Robot("Jeff", False, 101).charge(),
+            self.robot_power_on_battery_full.charge(),
             "I'm fully charged"
         )
 
     def test_charge_when_battery_not_full(self):
         self.assertEqual(
-            Robot("Jeff", False, 50).charge(),
+            self.robot_power_on_battery_average.charge(),
             "Gimme a minute to charge...\n DONE"
         )
 
-    # Testing Battery Status Method - Power ON
+    # Testing Charge Method - Power OFF ---------------------------------------------------------
+    def test_charge_when_battery_full_power_off(self):
+        self.assertEqual(
+            self.robot_power_off_battery_full.charge(),
+            "..."
+        )
+
+    def test_charge_when_battery_not_full_power_off(self):
+        self.assertEqual(
+            self.robot_power_off_battery_average.charge(),
+            "..."
+        )
+
+    # Testing Battery Status Method - Power ON ---------------------------------------------------
     def test_battery_full_status_power_on(self):
         self.assertEqual(
-            Robot("Jeff", True, 101).battery_status(),
+            self.robot_power_on_battery_full.battery_status(),
             "Battery: 100%\n"
             "I'm full!"
+        )
+        self.assertEqual(
+            self.robot_power_on_battery_full.battery, 100
         )
 
     def test_battery_low_status_power_on(self):
         self.assertLessEqual(
-            Robot("Jeff", True, 25).battery_status(),
-            "Battery: 25%\n"
+            self.robot_power_on_battery_lower.battery_status(),
+            "Battery: 24%\n"
             "I think i might need a little charge"
+        )
+        self.assertEqual(
+            self.robot_power_on_battery_lower.battery, 24
         )
 
     def test_battery_average_status_power_on(self):
         self.assertLessEqual(
-            Robot("Jeff", True, 99).battery_status(),
-            "Battery: 99%\n"
+            self.robot_power_on_battery_average.battery_status(),
+            "Battery: 49%\n"
+        )
+        self.assertEqual(
+            self.robot_power_on_battery_average.battery, 49
         )
 
-    # Testing Battery Status Method - Power OFF
+    # Testing Battery Status Method - Power OFF -----------------------------------------------------
     def test_battery_full_status_power_off(self):
         self.assertEqual(
-            Robot("Jeff", False, 100).battery_status(),
+            self.robot_power_off_battery_full.battery_status(),
             "..."
+        )
+        self.assertEqual(
+            self.robot_power_off_battery_full.battery, 101
         )
 
     def test_battery_low_status_power_off(self):
         self.assertLessEqual(
-            Robot("Jeff", False, 25).battery_status(),
+            self.robot_power_off_battery_lower.battery_status(),
             "..."
+        )
+        self.assertEqual(
+            self.robot_power_off_battery_lower.battery, 25
         )
 
     def test_battery_average_status_power_off(self):
         self.assertLessEqual(
-            Robot("Jeff", False, 99).battery_status(),
+            self.robot_power_off_battery_average.battery_status(),
             "..."
         )
+        self.assertEqual(
+            self.robot_power_off_battery_average.battery, 50
+        )
 
-    # Testing learn phrase - Power On
+    # Testing learn phrase - Power On ----------------------------------------------------------------
     def test_learn_phrase_add_phrase(self):
         self.assertEqual(
-            Robot("Jeff", True, 100).learn_phrase("PhraseOne"),
+            self.robot_power_on_battery_full.learn_phrase("uau"),
             'Phrase Added'
+        )
+        self.assertEqual(
+            self.robot_power_on_battery_full.battery, 96
         )
 
     def test_learn_phrase_add_same_phrase(self):
         self.assertEqual(
-            Robot("Jeff", True, 100, phrases=["PhraseOne"]).learn_phrase("PhraseOne"),
+            self.robot_power_on_battery_full.learn_phrase("PhraseOne"),
             'Phrase Added'
+        )
+        self.assertEqual(
+            self.robot_power_on_battery_full.battery, 96
         )
 
     def test_learn_phrase_limit(self):
         self.assertEqual(
-            Robot("Jeff", True, 100, phrases=[
-                "PhraseOne",
-                "PhraseTwo",
-                "PhraseThree",
-                "PhraseFour",
-                "PhraseFive",
-                "PhraseSix"]).learn_phrase("PhraseSeven"),
+            self.robot_memory_full_power_on.learn_phrase("PhraseSeven"),
             'Memory full'
         )
+        self.assertEqual(
+            self.robot_memory_full_power_on.battery, 101
+        )
 
-    # Testing learn phrase - Power Off
+    # Testing learn phrase - Power Off -------------------------------------------------------------
     def test_learn_phrase_add_phrase_power_off(self):
         self.assertEqual(
             Robot("Jeff", False, 100).learn_phrase("PhraseOne"),
             '...'
         )
+        self.assertEqual(
+            self.robot_memory_full_power_off.battery, 101
+        )
 
     def test_learn_phrase_add_same_phrase_power_off(self):
         self.assertEqual(
-            Robot("Jeff", False, 100, phrases=["PhraseOne"]).learn_phrase("PhraseOne"),
+            self.robot_power_off_battery_full.learn_phrase("PhraseOne"),
             '...'
+        )
+        self.assertEqual(
+            self.robot_power_off_battery_full.battery, 101
         )
 
     def test_learn_phrase_limit_power_off(self):
         self.assertEqual(
-            Robot("Jeff", False, 100, phrases=[
-                "PhraseOne",
-                "PhraseTwo",
-                "PhraseThree",
-                "PhraseFour",
-                "PhraseFive",
-                "PhraseSix"]).learn_phrase("PhraseSeven"),
+            self.robot_memory_full_power_off.learn_phrase("PhraseSeven"),
+            '...')
+        self.assertEqual(
+            self.robot_memory_full_power_off.battery, 101
+        )
+
+    # Testing show phrases_power on -----------------------------------------------------
+    def test_show_phrases_power_on(self):
+        self.assertEqual(
+            self.robot_memory_full_power_on.show_phrases(),
+            'Done'
+        )
+        self.assertEqual(
+            self.robot_memory_full_power_on.battery, 98
+        )
+
+    # Testing show phrases_power off -----------------------------------------------------
+    def test_show_phrases_power_off(self):
+        self.assertEqual(
+            self.robot_memory_full_power_off.show_phrases(),
             '...'
+        )
+        self.assertEqual(
+            self.robot_memory_full_power_off.battery, 101
+        )
+
+    # Testing show phrases_power on -----------------------------------------------------
+    def test_show_a_phrase_power_on(self):
+        self.assertEqual(
+            self.robot_memory_full_power_on.show_a_phrase(1),
+            'PhraseOne'
+        )
+        self.assertEqual(
+            self.robot_memory_full_power_on.battery, 98
+        )
+
+    def test_show_a_phrase_power_on_invalid_phrase_num(self):
+        self.assertEqual(
+            self.robot_memory_full_power_on.show_a_phrase(0),
+            'Phrase not found!\nbe sure you put the right phrase number...'
+        )
+        self.assertEqual(
+            self.robot_memory_full_power_on.battery, 98
+        )
+
+    # Testing show phrases_power off -----------------------------------------------------
+    def test_show_a_phrase_power_off(self):
+        self.assertEqual(
+            self.robot_memory_full_power_off.show_a_phrase(1),
+            '...'
+        )
+        self.assertEqual(
+            self.robot_memory_full_power_off.battery, 101
+        )
+
+    def test_show_a_phrase_power_off_invalid_phrase_num(self):
+        self.assertEqual(
+            self.robot_memory_full_power_off.show_a_phrase(1),
+            '...'
+        )
+        self.assertEqual(
+            self.robot_memory_full_power_off.battery, 101
         )
 
 
